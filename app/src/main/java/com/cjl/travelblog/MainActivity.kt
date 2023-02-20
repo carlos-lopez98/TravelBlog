a55b334f89c345ab09e4137c4338b8e18dabad82
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-02-20 14:48:56 -0800

    Adding Login Page

--- app/src/main/java/com/cjl/travelblog/MainActivity.kt
+++ app/src/main/java/com/cjl/travelblog/MainActivity.kt
@@ -2,10 +2,21 @@ package com.cjl.travelblog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.cjl.travelblog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //This method binds our xml layout to this activity
         setContentView(R.layout.activity_main)
+
+        //Now this binder object, gives us access to all our id, without having to explicitly
+        //Label them like below
+        val binder = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binder.root)
+
+
+        binder.mainTextView.text = "Hello Educative.io"
     }
 }
